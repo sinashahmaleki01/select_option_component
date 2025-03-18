230abf6ea9a2609e6aba20da0cedac450ea3d930
sina <sinashahmaleki01@gmail.com>
2025-03-18 21:38:33 +0330

    feat: generated and append html element with javascript

--- js/index.js
+++ js/index.js
@@ -1,35 +1,69 @@
-//=============|variables|===============// 
-let selected_container = document.querySelector(".selected-container")
-let selected_value = document.getElementById('selected');
-let list_options = document.querySelectorAll(".option");
-let icon = document.getElementById("icon")
 
-//================|addEvent Listener for selected container
-selected_container.addEventListener("click", function () {
-    toggleList()
-})
+(function () {
+    //================|
 
+    let container = document.querySelector('.container')
 
-//================|loop through list options and replace content when click on options
-list_options.forEach(option => {
-    option.addEventListener("click", function () {
-        selected_value.innerText = option.innerText
-        toggleList()
+    let select_container = document.createElement("div")
+
+    select_container.setAttribute("class", "select-container")
+
+    container.append(select_container)
+
+    select_container.innerHTML = `<div id = "selected">Select an option</div><i class="fa fa-angle-down" id="icon"></i>`
+
+    let selected_value = document.getElementById('selected');
+
+    //================|addEvent Listener for selected container
+    select_container.addEventListener("click", toggleList)
+
+    let list_options = [
+        "Mac Computers",
+        "Wearables and Accessories",
+        "AirPods & Audio Products",
+        "iCloud & iCloud+",
+        "Apple Pay & Apple Card",
+        "Apple Fitness+",
+        "Magic Keyboard, Magic Mouse",
+        "Apple TV HD (Discontinued)"
+    ]
+
+    let ul = document.createElement("ul")
+
+    ul.setAttribute("class", "options-container")
+
+    container.append(ul)
+
+    list_options.forEach((option) => {
+        ul.innerHTML += `<li>${option}</li>`
+
     })
-})
-//================|function: toggle List options
-function toggleList() {
-    $('.list-options').slideToggle(300);
-    icon.classList.toggle("angle-up")
 
-}
+    let options = document.querySelectorAll(".options-container li");
 
-//================|close the list if click outside of the list or selected container
-document.addEventListener("click", function(event) {
+    options.forEach((item) => {
+        item.addEventListener("click", function () {
+            selected_value.innerText = item.innerText
+            toggleList()
+        })
+    })
 
-    if (!selected_container.contains(event.target) && !document.querySelector('.list-options').contains(event.target)) {
-        $('.list-options').slideUp(300)
-        icon.classList.remove("angle-up")
+    let icon = document.getElementById("icon")
+
+    //================|function: toggle List options
+    function toggleList() {
+        $('.options-container').slideToggle(300);
+        icon.classList.toggle("angle-up")
     }
-    
-});
+
+    // //================|close the list if click outside of the list or selected container
+    document.addEventListener("click", function (event) {
+
+        if (!select_container.contains(event.target) && !document.querySelector('.options-container li').contains(event.target)) {
+            $('.options-container').slideUp(300)
+            icon.classList.remove("angle-up")
+        }
+
+    });
+})()
+
