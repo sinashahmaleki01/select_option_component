c97af4afaf5bf8b76cf3dd36ceac21e3ba83e068
sina <sinashahmaleki01@gmail.com>
2025-03-19 14:34:05 +0330

    feat: add initial function

--- js/index.js
+++ js/index.js
@@ -1,69 +1,72 @@
 
 (function () {
-    //================|
 
-    let container = document.querySelector('.container')
+    function initialize() {
+        let costumSelect = document.querySelector('.costumSelect')
 
-    let select_container = document.createElement("div")
+        let select_container = document.createElement("div")
 
-    select_container.setAttribute("class", "select-container")
+        select_container.setAttribute("class", "select-container")
 
-    container.append(select_container)
+        costumSelect.append(select_container)
 
-    select_container.innerHTML = `<div id = "selected">Select an option</div><i class="fa fa-angle-down" id="icon"></i>`
+        select_container.innerHTML = `<div id = "selected">Select an option</div><i class="fa fa-angle-down" id="icon"></i>`
 
-    let selected_value = document.getElementById('selected');
+        let selected_value = document.getElementById('selected');
 
-    //================|addEvent Listener for selected container
-    select_container.addEventListener("click", toggleList)
+        //================|addEvent Listener for selected container
+        select_container.addEventListener("click", toggleList)
 
-    let list_options = [
-        "Mac Computers",
-        "Wearables and Accessories",
-        "AirPods & Audio Products",
-        "iCloud & iCloud+",
-        "Apple Pay & Apple Card",
-        "Apple Fitness+",
-        "Magic Keyboard, Magic Mouse",
-        "Apple TV HD (Discontinued)"
-    ]
+        let list_options = [
+            "Mac Computers",
+            "Wearables and Accessories",
+            "AirPods & Audio Products",
+            "iCloud & iCloud+",
+            "Apple Pay & Apple Card",
+            "Apple Fitness+",
+            "Magic Keyboard, Magic Mouse",
+            "Apple TV HD (Discontinued)"
+        ]
 
-    let ul = document.createElement("ul")
+        let ul = document.createElement("ul")
 
-    ul.setAttribute("class", "options-container")
+        ul.setAttribute("class", "options-container")
 
-    container.append(ul)
+        costumSelect.append(ul)
 
-    list_options.forEach((option) => {
-        ul.innerHTML += `<li>${option}</li>`
+        list_options.forEach((option) => {
+            ul.innerHTML += `<li>${option}</li>`
 
-    })
-
-    let options = document.querySelectorAll(".options-container li");
-
-    options.forEach((item) => {
-        item.addEventListener("click", function () {
-            selected_value.innerText = item.innerText
-            toggleList()
         })
-    })
 
-    let icon = document.getElementById("icon")
+        let options = document.querySelectorAll(".options-container li");
 
+        options.forEach((item) => {
+            item.addEventListener("click", function () {
+                selected_value.innerText = item.innerText
+                toggleList()
+            })
+        })
+
+        let icon = document.getElementById("icon")
+
+        // //================|close the list if click outside of the list or selected container
+        document.addEventListener("click", function (event) {
+
+            if (!select_container.contains(event.target) && !document.querySelector('.options-container li').contains(event.target)) {
+                $('.options-container').slideUp(300)
+                icon.classList.remove("angle-up")
+            }
+
+        });
+
+    }
+    initialize()
     //================|function: toggle List options
     function toggleList() {
         $('.options-container').slideToggle(300);
         icon.classList.toggle("angle-up")
     }
-
-    // //================|close the list if click outside of the list or selected container
-    document.addEventListener("click", function (event) {
-
-        if (!select_container.contains(event.target) && !document.querySelector('.options-container li').contains(event.target)) {
-            $('.options-container').slideUp(300)
-            icon.classList.remove("angle-up")
-        }
-
-    });
+    
 })()
 
